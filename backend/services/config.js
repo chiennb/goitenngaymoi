da603b5ff9da980cc00b495371a6bc649b53bffc
Chiennb <chiennb@gmail.com>
2015-08-23 19:41:21 +0700

    Config

--- backend/services/config.js
+++ backend/services/config.js
@@ -1,14 +1,14 @@
 module.exports = {
-    APP_URL: 'http://localhost:' + 6969,
-    //APP_URL: 'http://gala2015.fis.vn',
+    //APP_URL: 'http://localhost:' + 6969,
+    APP_URL: 'http://gala2015.fis.vn',
     FACEBOOK_SECRET: '2c1bb2c2003e63c109f1d205c91bdf30',
     GOOGLE_SECRET: 'xjJ9wOaE2hcKGZcUBnso_XaO',
     EMAIL_SECRET: 'something',
     SMTP_SERVER: 'smtp.gmail.com',
     SMTP_USERNAME: 'fis.goitenngaymoi@gmail.com',
     SMTP_PASSWORD: 'abc@@123',
-    DB_CONNECTION: 'mongodb://localhost:27017/tickets',
-    //DB_CONNECTION: 'mongodb://UserReadWrite:Aa123aA@localhost:27017/UsersDB',
+    //DB_CONNECTION: 'mongodb://localhost:27017/tickets',
+    DB_CONNECTION: 'mongodb://UserReadWrite:Aa123aA@localhost:27017/UsersDB',
     PORT: 6969,
     TICKETS: 666
 };
