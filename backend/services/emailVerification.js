ea7e0adf5be94f66c6101e9a347b07529acc5ab9
Chiennb <chiennb@gmail.com>
2015-08-23 20:54:04 +0700

    Message

--- backend/services/emailVerification.js
+++ backend/services/emailVerification.js
@@ -59,6 +59,43 @@ exports.send =  function(email){
     } 
 };
 
+function sendSucess(email){
+    try{
+        
+        var transporter = nodemailer.createTransport(smtpTransport({
+            host: config.SMTP_SERVER,
+            secure: true,
+            auth: {
+                user: config.SMTP_USERNAME,
+                pass: config.SMTP_PASSWORD
+            }
+        }));
+
+        var mailOptions = {
+            from: 'Gala FIS 2015 <accounts@google.com>',
+            to: email,
+            subject: 'Ban da day ky thanh cong tham gia su kien  "Gala Goi Ngay Moi"',
+            //html: getHtml(token)
+            html: "<p>Bạn đã đăng ký công. Bạn hãy nhanh chân đén BTC để nhận vé.</p>"
+        };
+
+        transporter.sendMail(mailOptions, function(err, info){
+            if(err){
+                console.log("ERROR");
+                console.log(err);
+                //return res.status(500, err);
+                ;
+            }
+
+            console.log("OK");
+            console.log('email sent ', info.response);
+        });
+    } catch (Err) {
+        console.log("skipping: " + Err);
+        return; // continue
+    } 
+};
+
 exports.handler = function(req, res){
     var token = req.query.token;
 
