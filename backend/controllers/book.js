bf1c4c8d5b2ceed623546591ad92899592addcb7
Chiennb <chiennb@gmail.com>
2015-08-25 17:20:18 +0700

    Send mail 2

--- backend/controllers/book.js
+++ backend/controllers/book.js
@@ -3,25 +3,25 @@ var Book = require('../models/Book');
 var config = require('../services/config.js');
 var emailVerification = require('../services/emailVerification.js');
 // Create endpoint /api/books for POST
-exports.postBooks = function(req, res) {
-  // Create a new instance of the Book model
-  var book = new Book();
+exports.postBooks = function (req, res) {
+    // Create a new instance of the Book model
+    var book = new Book();
 
-  // Set the book properties that came from the POST data
-  book.email = req.body.email;
-  book.tickettype = req.body.tickettype;
+    // Set the book properties that came from the POST data
+    book.email = req.body.email;
+    book.tickettype = req.body.tickettype;
 
-  // Save the book and check for errors
-  book.save(function(err) {
-    if (err)
-      return err;
+    // Save the book and check for errors
+    book.save(function (err) {
+        if (err)
+            return err;
 
-    res.json(book);
-  });
+        res.json(book);
+    });
 };
 
 // Create endpoint /api/books for GET
-exports.getBooks = function(req, res) {
+exports.getBooks = function (req, res) {
 
     var perPage = config.PAGESIZE;
     var query = Book.find();
@@ -31,17 +31,17 @@ exports.getBooks = function(req, res) {
 
     if (req.query.email != null && req.query.email != '')
         query.where('email').equals(new RegExp(req.query.email, 'i'));
-        //query["email"] = new RegExp(req.query.email, 'i');
+    //query["email"] = new RegExp(req.query.email, 'i');
 
     if (req.query.status != null && req.query.status != '')
         query.where('status').equals(req.query.status);
-        //query["status"] = status;
+    //query["status"] = status;
 
     var page = 0;
     if (req.query.page != null && req.query.page != '')
         page = req.query.page - 1;
 
-  
+
     //console.log(page);
     query
     //Book.find(query)
@@ -62,64 +62,64 @@ exports.getBooks = function(req, res) {
             queryCount.where('status').equals(req.query.status);
 
         queryCount.count().exec(function (err, count) {
-            res.json({books: books, count: count});            
+            res.json({ books: books, count: count });
         })
 
-        
+
     });
 
 
-  //console.log(req.query.page);
-  //console.log(req.query.email);
-  //console.log(req.query.status);
-  //console.log(req.query.floor);
+    //console.log(req.query.page);
+    //console.log(req.query.email);
+    //console.log(req.query.status);
+    //console.log(req.query.floor);
 
-  // Use the Book model to find all book
-  //Book.find({}, function(err, books) {
-  //  if (err)
-  //    res.send(err);
+    // Use the Book model to find all book
+    //Book.find({}, function(err, books) {
+    //  if (err)
+    //    res.send(err);
 
-  //  res.json(books);
-  //});
+    //  res.json(books);
+    //});
 };
 
 // Create endpoint /api/books/:book_id for GET
-exports.getBook = function(req, res) {
-  // Use the Book model to find a specific book
-  Book.findOne({_id: req.params.book_id }, function(err, book) {
-    if (err)
-      res.send(err);
+exports.getBook = function (req, res) {
+    // Use the Book model to find a specific book
+    Book.findOne({ _id: req.params.book_id }, function (err, book) {
+        if (err)
+            res.send(err);
 
-    res.json(book);
-  });
+        res.json(book);
+    });
 };
 
 // Create endpoint /api/books/:book_id for PUT
-exports.putBook = function(req, res) {
-   //Use the Book model to find a specific book
-   Book.update({ _id: req.params.book_id }, { tickettype: req.body.tickettype, status: req.body.status }, function(err, num, raw) {
-     if (err)
-       res.send(err);
+exports.putBook = function (req, res) {
+    //Use the Book model to find a specific book
+    Book.update({ _id: req.params.book_id }, { tickettype: req.body.tickettype, status: req.body.status }, function (err, num, raw) {
+        if (err)
+            res.send(err);
 
-     res.json({ message: num + ' updated' });
-   });
+        res.json({ message: num + ' updated' });
+    });
 };
 
 // Create endpoint /api/books/:book_id for DELETE
-exports.deleteBook = function(req, res) {
-  // Use the Book model to find a specific book and remove it
-  Book.remove({ _id: req.params.book_id }, function(err) {
-    if (err)
-      res.send(err);
+exports.deleteBook = function (req, res) {
+    // Use the Book model to find a specific book and remove it
+    Book.remove({ _id: req.params.book_id }, function (err) {
+        if (err)
+            res.send(err);
 
-    res.json({ message: 'Book removed from the locker!' });
-  });
+        res.json({ message: 'Book removed from the locker!' });
+    });
 };
 
 
 // Create endpoint /api/books/:book_id for DELETE
 exports.sendMailPending = function (req, res) {
-    
+
     var query = Book.find();
 
     query.where('status').equals('pending');
@@ -129,12 +129,26 @@ exports.sendMailPending = function (req, res) {
     })
     .exec(function (err, books) {
         if (err)
-            res.send(err);        
+            res.send(err);
 
-        books.forEach(function (entry) {
-            //console.log(entry.email);
-            emailVerification.send(entry.email);
-        });
+        //books.forEach(function (entry) {
+        //    //console.log(entry.email);
+        //    try{
+        //        emailVerification.send(entry.email);
+        //    } catch (Err) {
+        //        console.log("skipping: " + Err);
+        //        continue;
+        //    }
+        //});
+        for (var i = 0, len = books.length; i < len; i++) {
+            try {
+                console.log('Send mail: ' + books[i].email);
+                emailVerification.send(books[i].email);
+            } catch (e) {
+                console.log(e);
+                continue;
+            }
+        }
 
         res.send('Ok');
     });
