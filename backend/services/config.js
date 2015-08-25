ee026b67a7caec5488455142eddac562f9ff3780
Chiennb <chiennb@gmail.com>
2015-08-25 10:46:25 +0700

    Update floor

--- backend/services/config.js
+++ backend/services/config.js
@@ -7,7 +7,7 @@ module.exports = {
     SMTP_SERVER: 'smtp.gmail.com',
     SMTP_USERNAME: 'fis.goitenngaymoi@gmail.com',
     SMTP_PASSWORD: 'abc@@123',
-    DB_CONNECTION: 'mongodb://localhost:27017/tickets',
+    //DB_CONNECTION: 'mongodb://localhost:27017/tickets',
     DB_CONNECTION: 'mongodb://UserReadWrite:Aa123aA@localhost:27017/UsersDB',
     PAGESIZE: 1,
     PORT: 6969,
